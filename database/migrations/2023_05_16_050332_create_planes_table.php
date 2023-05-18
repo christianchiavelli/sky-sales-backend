df05d5612d88d7b73de162a15c7e20e8781b43d4
Christian Chiavelli <christian.chiaveli@valtech.com>
2023-05-18 11:01:08 -0300

    feat: Create initial project structure

---
+++ database/migrations/2023_05_16_050332_create_planes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlanesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('planes', function (Blueprint $table) {
+            $table->id();
+            $table->string('model');
+            $table->string('manufacturer');
+            $table->year('year');
+            $table->integer('seats');
+            $table->integer('runway_length');
+            $table->decimal('price', 8, 2);
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('planes');
+    }
+}
