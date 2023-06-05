77713a002677f3f8f6502d57ca1eaba0d0d499a4
Christian Chiavelli <christian.chiaveli@valtech.com>
2023-06-05 17:35:03 -0300

    feat(ui): enhance overall UI by updating 'About Us' layout

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,25 +6,25 @@
 		<div class="col-lg-8">
 			<div class="text-center mb-5">
 				<h1 class="display-4">Quem Somos</h1>
-				<p class="lead">
+				<p class="lead text-secondary">
 					A Loja de Aviões é uma empresa líder no setor aeroespacial, com mais de 20 anos de experiência. Nossa missão é
 					conectar pessoas através de soluções de aviação excepcionais.
 				</p>
 			</div>
-			<div class="card mb-4 shadow">
+			<div class="card mb-5 shadow">
 				<div class="card-body">
-					<h3 class="card-title">Nossa História</h3>
-					<p class="card-text">
+					<h3 class="card-title mb-3">Nossa História</h3>
+					<p class="card-text text-secondary">
 						Fundada em 2003, a Loja de Aviões começou como uma pequena loja e cresceu para se tornar uma das maiores
 						fornecedoras de aviões do país. Nós nos orgulhamos de nosso compromisso com a qualidade e o atendimento ao
 						cliente.
 					</p>
 				</div>
 			</div>
-			<div class="card mb-4 shadow">
+			<div class="card mb-5 shadow">
 				<div class="card-body">
-					<h3 class="card-title">Nossa Equipe</h3>
-					<p class="card-text">
+					<h3 class="card-title mb-3">Nossa Equipe</h3>
+					<p class="card-text text-secondary">
 						Nossa equipe é composta por profissionais apaixonados e experientes na indústria da aviação. Cada membro da
 						nossa equipe está comprometido em fornecer a melhor experiência para nossos clientes.
 					</p>
@@ -32,15 +32,21 @@
 			</div>
 			<div class="card shadow">
 				<div class="card-body">
-					<h3 class="card-title">Nossos Valores</h3>
+					<h3 class="card-title mb-3">Nossos Valores</h3>
 					<ul class="list-group list-group-flush">
-						<li class="list-group-item"><strong>Compromisso com a qualidade:</strong> Nós nos esforçamos para oferecer
+						<li class="list-group-item text-secondary"><strong>Compromisso com a qualidade:</strong> Nós nos esforçamos
+							para
+							oferecer
 							os melhores produtos e serviços no mercado.</li>
-						<li class="list-group-item"><strong>Inovação contínua:</strong> Estamos constantemente buscando formas de
+						<li class="list-group-item text-secondary"><strong>Inovação contínua:</strong> Estamos constantemente
+							buscando
+							formas de
 							melhorar nossos produtos e processos.</li>
-						<li class="list-group-item"><strong>Integridade:</strong> Conduzimos nossos negócios de forma ética e
+						<li class="list-group-item text-secondary"><strong>Integridade:</strong> Conduzimos nossos negócios de forma
+							ética e
 							responsável.</li>
-						<li class="list-group-item"><strong>Serviço excepcional ao cliente:</strong> Estamos comprometidos em
+						<li class="list-group-item text-secondary"><strong>Serviço excepcional ao cliente:</strong> Estamos
+							comprometidos em
 							superar as expectativas dos nossos clientes.</li>
 					</ul>
 				</div>
